eb02a562aa5932bf5ceb4fe9cd9241d52bd99e44
deo5inco <56805898+deo5inco@users.noreply.github.com>
2024-10-04 11:27:30 -0600

    Codigo Fuente de phone and taxi
    aqui encontraras el ejemplo que realice sobre la interface AD102 para obtener solo el numero de telefono de la llamada que entra,

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace PhoneAndTaxi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Linea1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CopiarLinea1 = new System.Windows.Forms.Button();
+            this.Linea2 = new System.Windows.Forms.GroupBox();
+            this.CopiarLinea2 = new System.Windows.Forms.Button();
+            this.Linea1.SuspendLayout();
+            this.Linea2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Linea1
+            // 
+            this.Linea1.Controls.Add(this.label1);
+            this.Linea1.Controls.Add(this.CopiarLinea1);
+            this.Linea1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Linea1.Location = new System.Drawing.Point(12, 18);
+            this.Linea1.Name = "Linea1";
+            this.Linea1.Size = new System.Drawing.Size(530, 180);
+            this.Linea1.TabIndex = 0;
+            this.Linea1.TabStop = false;
+            this.Linea1.Text = "Linea 1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(445, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Estado";
+            // 
+            // CopiarLinea1
+            // 
+            this.CopiarLinea1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CopiarLinea1.Location = new System.Drawing.Point(6, 46);
+            this.CopiarLinea1.Name = "CopiarLinea1";
+            this.CopiarLinea1.Size = new System.Drawing.Size(515, 105);
+            this.CopiarLinea1.TabIndex = 1;
+            this.CopiarLinea1.Text = "L1";
+            this.CopiarLinea1.UseVisualStyleBackColor = true;
+            this.CopiarLinea1.Click += new System.EventHandler(this.CopiarLinea1_Click);
+            // 
+            // Linea2
+            // 
+            this.Linea2.Controls.Add(this.CopiarLinea2);
+            this.Linea2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Linea2.Location = new System.Drawing.Point(548, 18);
+            this.Linea2.Name = "Linea2";
+            this.Linea2.Size = new System.Drawing.Size(530, 180);
+            this.Linea2.TabIndex = 1;
+            this.Linea2.TabStop = false;
+            this.Linea2.Text = "Linea 2";
+            // 
+            // CopiarLinea2
+            // 
+            this.CopiarLinea2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CopiarLinea2.Location = new System.Drawing.Point(16, 46);
+            this.CopiarLinea2.Name = "CopiarLinea2";
+            this.CopiarLinea2.Size = new System.Drawing.Size(515, 105);
+            this.CopiarLinea2.TabIndex = 2;
+            this.CopiarLinea2.Text = "L2";
+            this.CopiarLinea2.UseVisualStyleBackColor = true;
+            this.CopiarLinea2.Click += new System.EventHandler(this.CopiarLinea2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1094, 215);
+            this.Controls.Add(this.Linea2);
+            this.Controls.Add(this.Linea1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Phone And Taxi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Linea1.ResumeLayout(false);
+            this.Linea1.PerformLayout();
+            this.Linea2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Linea1;
+        private System.Windows.Forms.GroupBox Linea2;
+        private System.Windows.Forms.Button CopiarLinea1;
+        private System.Windows.Forms.Button CopiarLinea2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace PhoneAndTaxi
+{
+    public struct AD101DEVICEPARAMETER
+    {
+        public int nRingOn;
+        public int nRingOff;
+        public int nHookOn;
+        public int nHookOff;
+        public int nStopCID;
+        public int nNoLine;         // Add this parameter in new AD101(MCU Version is 6.0)
+    }
+    public partial class Form1 : Form
+    {
+        [DllImport("AD101Device.dll", EntryPoint = "AD101_GetCallerID", CharSet = CharSet.Ansi)]
+        public static extern int AD101_GetCallerID(int nLine, StringBuilder szCallerIDBuffer, StringBuilder szName, StringBuilder szTime);
+
+        // Control led 
+        [DllImport("AD101Device.dll", EntryPoint = "AD101_SetLED")]
+        public static extern int AD101_SetLED(int nLine, int enumLed);
+
+        [DllImport("AD101Device.dll", EntryPoint = "AD101_GetParameter")]
+        public static extern int AD101_GetParameter(int nLine, ref AD101DEVICEPARAMETER tagParameter);
+
+
+        [DllImport("AD101Device.dll", EntryPoint = "AD101_InitDevice")]
+        public static extern int AD101_InitDevice(int hWnd);
+
+        // Get talking time
+        [DllImport("AD101Device.dll", EntryPoint = "AD101_GetTalkTime")]
+        public static extern int AD101_GetTalkTime(int nLine);
+
+
+        [DllImport("AD101Device.dll", EntryPoint = "AD101_ReadParameter")]
+        public static extern int AD101_ReadParameter(int nLine);
+
+        public const int MCU_BACKCID = 0x09;		// Return Device CallerID
+        public const int WM_USBLINEMSG = 1024 + 180;
+        public const int MCU_BACKTALK = 0xBB;
+        public const int MCU_BACKPARAM = 0x0C;
+        public const int MCU_BACKID = 0x07;	// Return Device ID
+        //public const int MCU_BACKSTATE = 0x08;	// Return Device State
+        //public const int MCU_BACKCPUID = 0x0D;	// Return Device CPU ID
+        //public const int MCU_BACKPARAM = 0x0C;	// Return Device Paramter
+        //public const int MCU_BACKDEVICE = 0x0B;	// Return Device Back Device ID
+        // LED Status 
+        enum LEDTYPE
+        {
+            LED_CLOSE = 1,
+            LED_RED,
+            LED_GREEN,
+            LED_YELLOW,
+            LED_REDSLOW,
+            LED_GREENSLOW,
+            LED_YELLOWSLOW,
+            LED_REDQUICK,
+            LED_GREENQUICK,
+            LED_YELLOWQUICK,
+        };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void DefWndProc(ref System.Windows.Forms.Message m)
+        {
+            try
+            {
+
+                //    OnDeviceMsg(m.WParam, m.LParam);
+                switch (m.Msg)
+                {
+                    case WM_USBLINEMSG: //´¦ÀíÏûÏ¢¡¡
+                        OnDeviceMsg(m.WParam, m.LParam);
+                        break;
+                    default:
+                        base.DefWndProc(ref m);//µ÷ÓÃ»ùÀàº¯Êý´¦Àí·Ç×Ô¶¨ÒåÏûÏ¢¡£¡¡¡¡¡¡
+                        break;
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void OnDeviceMsg(IntPtr wParam, IntPtr Lparam)
+        {
+            try
+            {
+                int nMsg = new int();
+                int nLine = new int();
+
+                nMsg = wParam.ToInt32() % 65536;
+                nLine = wParam.ToInt32() / 65536;
+
+                switch (nMsg)
+                {
+                    ////
+                    case MCU_BACKID:
+                        {
+                            StringBuilder szCPUVersion = new StringBuilder(32);
+                    
+                            label1.Text = "Enable";
+
+                           //AD101_GetCPUVersion(nLine, szCPUVersion);
+
+
+                      }
+                    break;
+                    case MCU_BACKCID:
+                        {
+                            StringBuilder szCallerID = new StringBuilder(128);
+                            StringBuilder szName = new StringBuilder(128);
+                            StringBuilder szTime = new StringBuilder(128);
+
+                            int nLen = AD101_GetCallerID(nLine, szCallerID, szName, szTime);
+                            string check = "";
+                            check = szCallerID.ToString();
+
+
+                            this.BeginInvoke((System.Windows.Forms.MethodInvoker)delegate ()
+                            {
+                                if (check == null)
+                                {
+                                    CopiarLinea1.Text = "International Number";
+
+                                }
+                                else
+                                {
+                                    string str = szCallerID.ToString();
+                                    string newString = Regex.Replace(str, "[^.0-9]", "");
+
+                                    CopiarLinea1.Text = newString;
+
+                                }
+
+                            });
+                        }
+                        break;
+                    case MCU_BACKTALK:
+                        {
+                            string strTalk;
+                            strTalk = string.Format("{0:D2}", Lparam) + "S";
+                            //listView1.Items[nLine].SubItems[7].Text = strTalk;
+
+                        }
+                        break;
+                    case MCU_BACKPARAM:
+                        {
+                            AD101DEVICEPARAMETER tagParameter = new AD101DEVICEPARAMETER();
+
+                            AD101_GetParameter(nLine, ref tagParameter);
+                        }
+                        break;
+                    //case MCU_BACKCPUID:
+                    //    {
+                    //        StringBuilder szCPUID = new StringBuilder(4);
+
+                    //        AD101_GetCPUID(nLine, szCPUID);
+
+                    //        //listView1.Items[nLine].SubItems[8].Text = szCPUID.ToString();
+
+                    //    }
+                    //    break;
+                    //case MCU_BACKPARAM:
+                    //    {
+                    //        AD101DEVICEPARAMETER tagParameter = new AD101DEVICEPARAMETER();
+
+                    //        AD101_GetParameter(nLine, ref tagParameter);
+                    //    }
+                    //    break;
+                    //case MCU_BACKDEVICE:
+                    //    {
+                    //        StringBuilder szCPUVersion = new StringBuilder(32);
+
+                    //        AD101_GetCPUVersion(nLine, szCPUVersion);
+
+
+                    //    }
+                    //    break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+
+
+        private void CopiarLinea1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText (CopiarLinea1.Text);
+        }
+
+        private void CopiarLinea2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(CopiarLinea2.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (AD101_InitDevice(Handle.ToInt32()) == 0)
+                {
+                    return;
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message + "  " + ex.InnerException.Message); }
+            AD101_SetLED(0, 3);
+        }
+    }
+}
